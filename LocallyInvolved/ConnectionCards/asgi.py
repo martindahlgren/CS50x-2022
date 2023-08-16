4285f891afc27e402d144f98c2a194aad2e5f735
Martin Dahlgren <18004887+martindahlgren@users.noreply.github.com>
2023-08-16 10:51:09 +0200

    Rename app

--- LocallyInvolved/ConnectionCards/asgi.py
+++ LocallyInvolved/ConnectionCards/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for LocallyInvolved project.
+ASGI config for ConnectionCards project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocallyInvolved.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConnectionCards.settings')
 
 application = get_asgi_application()
--- LocallyInvolved/ConnectionCards/wsgi.py
+++ LocallyInvolved/ConnectionCards/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LocallyInvolved project.
+WSGI config for ConnectionCards project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocallyInvolved.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConnectionCards.settings')
 
 application = get_wsgi_application()
