7bbc2b59dfb88ac9d4a083b48fde71282d112c34
Martin Dahlgren <18004887+martindahlgren@users.noreply.github.com>
2023-09-30 18:30:15 +0200

    Rename

--- ConnectionCards/app/views.py
+++ ConnectionCards/app/views.py
@@ -1,5 +1,6 @@
 import json
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .util import cities
 import time
 
@@ -7,30 +8,39 @@ import time
 def index(request):
     return render(None, "app/index.html")
 
+@login_required
 def upload_picture(request):
     pass
 
+@login_required
 def get_matches(request):
     pass
 
+@login_required
 def get_chat(request):
     pass
 
+@login_required
 def send_chat(request):
     pass
 
+@login_required
 def get_more_messages(request):
     pass
 
+@login_required
 def get_candidates(request):
     pass
 
+@login_required
 def get_my_profile(request):
     pass
 
+@login_required
 def update_profile(request):
     pass
 
+@login_required
 def block_user(request):
     pass
 
@@ -38,9 +48,3 @@ def suggest_cities(request):
     data = json.loads(request.body)
     matches = cities.get_matches(data.city)
     pass # Based on partial city name
-
-start = time.time()
-print("\n\n")
-print(tuple(zip(*cities.get_matches("sto", 5)))[0])
-print("\n\n")
-print(time.time() - start)
